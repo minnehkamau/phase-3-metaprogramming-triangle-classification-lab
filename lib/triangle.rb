f7c587674517cb59db491e28430ddf02808787ae
minnehkamau <minnieches08@gmail.com>
2023-02-15 23:10:03 +0300

    Complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError; end
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3].sort
+    raise TriangleError if @sides.any?(&:zero?) || @sides[0] + @sides[1] <= @sides[2]
+  end
+
+  def kind
+    return :equilateral if @sides.uniq.size == 1
+    return :isosceles if @sides.uniq.size == 2
+    :scalene
+  end
+
 end
